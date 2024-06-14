0383b20c32dd95699a21f18700c6e5f435ada66e
Banu Salman Farisi <banu.farisi@jalin.co.id>
2024-06-14 09:05:17 +0700

    initial commit

---
+++ source_code/main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Customer Churn Predictions Using Classification Analysis')
+st.subheader('Test your subscriber potensial churn')
+
+with st.form("form data subscriber"):
+    
